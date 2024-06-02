9b63c3fdd6dd15e2539a593cb03f134030211cc3
SergeKryv <krivosheevsrg@gmail.com>
2024-06-02 08:35:06 +0300

    Custom user profile

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'custom_user_app.apps.CustomUserAppConfig',
     'members_app.apps.MembersAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
