6ebf213de85944aad112a80713ae236c61628804
SergeKryv <krivosheevsrg@gmail.com>
2024-07-22 16:46:30 +0200

    modified template to add status. Add simple Javascript to update status when page is loaded

--- mysite/messenger/static/messenger/js/chat_utils.js
+++ mysite/messenger/static/messenger/js/chat_utils.js
@@ -1,3 +1,19 @@
 // Function for scroll to be in the bottom by default
 var messageArea = document.querySelector('#messageArea');
         messageArea.scrollTop = messageArea.scrollHeight - messageArea.clientHeight;
+
+// Online checker
+// Function to run when the page has fully loaded
+function setStatus() {
+
+    // Select all status elements
+    const statusElements = document.querySelectorAll('.status');
+
+    // Iterate over each element and change its text content
+    statusElements.forEach(function(element) {
+        element.textContent = 'onfline';
+    });
+}
+
+// Attach function to the window onload event
+window.onload = setStatus;
